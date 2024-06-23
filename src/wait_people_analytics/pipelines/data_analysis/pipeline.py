fd0f1fc5b2b54b474cae1134a56ae11b9dafe21a
aleks <alekskarbovskiy@gmail.com>
2024-06-23 11:43:58 +0200

    Correlation Matrix Added

--- src/wait_people_analytics/pipelines/data_analysis/pipeline.py
+++ src/wait_people_analytics/pipelines/data_analysis/pipeline.py
@@ -1,7 +1,8 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import (
-    generate_corr_matrix_visual
+    generate_corr_matrix_visual,
+    filter_project_participants
 )
 
 
@@ -9,8 +10,14 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=generate_corr_matrix_visual,
+                func=filter_project_participants,
                 inputs="processed_survey",
+                outputs="filter_project_participants_data",
+                name="filter_project_participants",
+            ),
+            node(
+                func=generate_corr_matrix_visual,
+                inputs="filter_project_participants_data",
                 outputs="corr_matrix_visual",
                 name="generate_corr_matrix_visual",
             )
