f769447aeac3637bf5992eabad85ccb74d1895dd
aleks <alekskarbovskiy@gmail.com>
2024-06-23 11:43:58 +0200

    Correlation Matrix Added

--- src/wait_people_analytics/pipelines/data_analysis/nodes.py
+++ src/wait_people_analytics/pipelines/data_analysis/nodes.py
@@ -5,9 +5,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def filter_project_participants(df: pd.DataFrame) -> Any:
+    df = df[df["Participant"] == 1]
+    return df
+
 def generate_corr_matrix_visual(df: pd.DataFrame) -> Any:
     numeric_df = df.select_dtypes(include=[np.number])
-    corr_matrix = df.corr()
+    corr_matrix = numeric_df.corr(method='spearman')
 
     plt.figure(figsize=(25, 20))
     mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
