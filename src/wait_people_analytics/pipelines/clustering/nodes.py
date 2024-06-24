0b87b29251d22280bfec64a33e182ee4ce6104d3
Wojtini <wmaziarzw@gmail.com>
2024-06-24 19:09:07 +0200

    style typos

--- src/wait_people_analytics/pipelines/clustering/nodes.py
+++ src/wait_people_analytics/pipelines/clustering/nodes.py
@@ -2,13 +2,12 @@ from typing import Any
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import scipy.cluster.hierarchy as sch
 
 
 def generate_hierarchical_clustering_visual(df: pd.DataFrame) -> Any:
-    filtered_df = df.drop(columns= "ID")
-    filtered_df = filtered_df.replace( {0: 1, 1:0})
+    filtered_df = df.drop(columns="ID")
+    filtered_df = filtered_df.replace({0: 1, 1: 0})
     clustering = sch.linkage(filtered_df, method="ward")
     plt.figure(figsize=(10, 4))
     sch.dendrogram(clustering)
