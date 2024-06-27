fdca8f0a0507a48590ddb1b739c3dfe9a7f14303
Wojtini <wmaziarzw@gmail.com>
2024-06-27 18:35:50 +0200

    Add docstrings to all nodes

--- src/wait_people_analytics/pipelines/insights/nodes.py
+++ src/wait_people_analytics/pipelines/insights/nodes.py
@@ -4,37 +4,73 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def generate_overall_stackedbar_visual(
+def generate_overall_stacked_bar_visual(
     df: pd.DataFrame,
     analytics_legend_mapping: dict[int, str],
 ) -> Any:
+    """
+    Generates stacked bar plot to show skill distribution across all skills
+
+    Args:
+        df: dataframe
+        analytics_legend_mapping: legend labels
+    Returns:
+        stacked bar plot
+    """
     df = _get_skill_count(df, analytics_legend_mapping)
     df = df.sort_values(by=4, ascending=False)
     return _get_bar_plot(df, analytics_legend_mapping, "Count of People with Each Skill Value")
 
 
-def generate_top_interested_stackedbar_visual(
+def generate_top_interested_stacked_bar_visual(
     df: pd.DataFrame,
     analytics_legend_mapping: dict[int, str],
 ) -> Any:
+    """
+    Generates stacked bar plot to show top skills which the most people are interested in
+
+    Args:
+        df: dataframe
+        analytics_legend_mapping: legend labels
+    Returns:
+        stacked bar plot
+    """
     df = _get_skill_count(df, analytics_legend_mapping)
     df = df.sort_values(by=2, ascending=False)
     return _get_bar_plot(df, analytics_legend_mapping, "Top by Count of Interested", top_n=8)
 
 
-def generate_top_not_interested_stackedbar_visual(
+def generate_top_not_interested_stacked_bar_visual(
     df: pd.DataFrame,
     analytics_legend_mapping: dict[int, str],
 ) -> Any:
+    """
+    Generates stacked bar plot to show top skills which the most people are not interested in
+
+    Args:
+        df: dataframe
+        analytics_legend_mapping: legend labels
+    Returns:
+        stacked bar plot
+    """
     df = _get_skill_count(df, analytics_legend_mapping)
     df = df.sort_values(by=1, ascending=False)
     return _get_bar_plot(df, analytics_legend_mapping, "Top by Count of Not Interested", top_n=8)
 
 
-def generate_top_unconscious_stackedbar_visual(
+def generate_top_unaware_stacked_bar_visual(
     df: pd.DataFrame,
     analytics_legend_mapping: dict[int, str],
 ) -> Any:
+    """
+    Generates stacked bar plot to show top skills which the most people are not aware
+
+    Args:
+        df: dataframe
+        analytics_legend_mapping: legend labels
+    Returns:
+        stacked bar plot
+    """
     df = _get_skill_count(df, analytics_legend_mapping)
     df = df.sort_values(by=0, ascending=False)
     return _get_bar_plot(df, analytics_legend_mapping, "Top by Count of Unconscious", top_n=8)
